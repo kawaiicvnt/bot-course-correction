5da63ddc1d26ce94df26721970875e1dfa92f41d
Evie (Ivi) Ballou <iviballou@gmail.com>
2026-01-12 12:53:22 +0200

    clean up main

--- v2/source/main.cpp
+++ v2/source/main.cpp
@@ -1,34 +1,38 @@
 // external deps
+#include "CodalFiber.h"
 #include "Compass.h"
 #include "Magnetometer.h"
 #include "ManagedString.h"
 #include "MicroBit.h"
+#include "MicroBitDevice.h"
 #include "MicroBitEvent.h"
 #include "MicroBitUARTService.h"
 #include <cstdint>
+#include <stdio.h>
 // internal deps
 #include "AlphaBot2.h"
-#include "Magnetometer.h"
 #include "Course.h"
 #include "Debug.h"
 
+#define DEBUGLINEWIDTH 60
+
 // Assuming AlphaBot2 constructor takes no arguments or different arguments
 AlphaBot2 alphabot;
-mag_acc_data mad; // Initialize the class holding our data hostage
-MicroBitUARTService *ble_uart;
-MicroBit uBit;
-MicroBitCompass *compass;
-MicroBitAccelerometer *accelerometer;
-// MicroBitRadio *radio;
+mag_acc_data mad; // M.A.D. - Magnetometer and Accelerometer Data
+MicroBitUARTService *ble_uart; // The BLE UART service. We can send and receive serial data to and from the connected device, by using this service.
+MicroBit uBit; // The microbit board definition
+
+// Default calibration data
+CompassCalibration* def_calib = new CompassCalibration(
+    Sample3D(-30716, 36320, -60056),
+    Sample3D(1096, 1103, 1093),
+    65903
+    );
 
 int speed = 0;
 float steer_weight = 1;
-Timer c_inactivity();
 
-CoordinateSpace uprightFacingAway(SIMPLE_CARTESIAN, false, COORDINATE_SPACE_ROTATED_180);
-
-struct debug_bot_state {
-  mag_acc_data mad;
+struct robot_speed {
   int16_t speed_l;
   int16_t speed_r;
 };
@@ -47,13 +51,6 @@ int fromHex(ManagedString str) {
     }
     return value;
 }
-
-
-// void onRadioReceive(MicroBitEvent) {
-//     PacketBuffer packet = uBit.radio.datagram.recv();
-//     ManagedString data = ManagedString((char *) packet.getBytes());
-//     printf(ManagedString("> Radio received -> ") + data.substring(0,5) + "\n> Translated data -> Dir: " + data.substring(0,1) + " | Speed: " + fromHex(data.substring(1, 2)) + " | Tilt: " + fromHex(data.substring(3, 2)) + "\n");
-// }
 
 struct Speed {
     int speed_l;
@@ -97,8 +94,6 @@ Speed determine_motor_speed_moving(int tar_speed, int tilt, bool forward) {
 }
 
 void move(MicroBitEvent) {
-    // PacketBuffer packet = uBit.radio.datagram.recv();
-    // ManagedString data = ManagedString((char *) packet.getBytes());
     ManagedString data = ble_uart->readUntil(ManagedString("\r\n"), SYNC_SLEEP);
     printf(ManagedString("> BLE received: ") + data);
     if (data == "LS") {
@@ -124,7 +119,7 @@ void move(MicroBitEvent) {
         printf(ManagedString("--> B | Speed -> L: ") + speed.speed_l + ManagedString(" | R: ") + speed.speed_r);
         alphabot.MotorRun(Motors::M1, -speed.speed_r);
         alphabot.MotorRun(Motors::M2, -speed.speed_l);
-    } //else if (c_inactivity)
+    }
     uBit.display.printCharAsync('O', 100);
 }
 
@@ -135,25 +130,18 @@ void changeSpeed(MicroBitEvent) {
 
 int main()
 {
-    printf("> Initializing uBit... ");
     uBit.init();
-    printf("Done!\n");
+    printf(ManagedString("\n") + ( DEBUGLINEWIDTH * ManagedString("-") ) + "\n"); // Line separator
+    printf("> Initialized uBit!\n");
 
+    // This is data we've extracted via debugging tools from a running
     printf("> Setting up pre-calculated calibration data... ");
-    CompassCalibration* calibration = new CompassCalibration();
-    calibration->centre = Sample3D(-30716, 36320, -60056);
-    calibration->scale = Sample3D(1096, 1103, 1093);
-    calibration->radius = 65903;
-    uBit.compass.setCalibration(*calibration);
+    uBit.compass.setCalibration(*def_calib);
     printf("Done!\n");
 
     // Set the rotation of the device, to upright, facing away from the user.
     // To ensure that we calculate the correct compass heading.
-    printf("> Setting up compass and accelerometer... ");
-    accelerometer = new MicroBitAccelerometer(uBit.i2c, uprightFacingAway);
-    compass = new MicroBitCompass(uBit.i2c, uprightFacingAway);
-    accelerometer->autoDetect(uBit.i2c);
-    compass->autoDetect(uBit.i2c);
+    printf("> Setting up magnetometer and accelerometer fiber... ");
     mad.init();
     create_fiber(update_gauss_data_fiber);
     printf("Done!\n");
@@ -163,25 +151,15 @@ int main()
     printf("Done!\n");
 
     printf("> Setting up BLE UART service... ");
-    //uBit.bleManager.init("botto(m)", uBit.getSerial(), uBit.messageBus, uBit.storage, false);
-    //uBit.bleManager.advertise();
-    //uBit.bleManager.setTransmitPower(7);
+    uBit.bleManager.init("botto(m)", uBit.getSerial(), uBit.messageBus, uBit.storage, false);
+    uBit.bleManager.setTransmitPower(7);
+    uBit.bleManager.advertise();
     ble_uart = new MicroBitUARTService(*uBit.ble, 32, 32);
     ble_uart->eventOn("\r\n");
     printf("Done!\n");
 
-    // printf("> Setting up radio fiber... ");
-    // create_fiber([]() {
-    //     uBit.radio.setGroup(1);
-    //     uBit.radio.setTransmitPower(7);
-    //     uBit.radio.setFrequencyBand(0);
-    //     uBit.radio.enable();
-    // });
-    // printf("Done!\n");
 
     printf("> Setting up event listeners... ");
-    // uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, ble_button_a);
-
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, [](MicroBitEvent) {
         Robot robot;
         // We wanna stay still and let the AB2 correct it's heading.
@@ -205,27 +183,17 @@ int main()
         alphabot.MotorRun(Motors::M2, 0);
         uBit.sleep(100);
     });
-
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, [](MicroBitEvent) {
         alphabot.MotorRun(Motors::M1, 0);
         alphabot.MotorRun(Motors::M2, 0);
     });
-
-    // uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, move);
-
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, [](MicroBitEvent) {
         uBit.compass.calibrate();
     });
-
     uBit.messageBus.listen(MICROBIT_ID_BLE_UART, MICROBIT_UART_S_EVT_DELIM_MATCH, move);
     printf("Done!\n");
 
-    printf("> Starting compass clock fiber... ");
-    // create_fiber(compass_clock_fiber);
-    printf("Done!\n");
-
-
-    printf("> Starting event loop...\n");
+    printf("> Finished setup!\n");
+    printf(( DEBUGLINEWIDTH * ManagedString("-") ) + "\n"); // Line separator
     release_fiber();
-    printf("Done!\n");
 }
